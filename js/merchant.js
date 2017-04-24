07cf00b1a6ca3060d81c3fff9a4568cc214b87e3
InnerAc <anjicun@live.com>
2017-04-24 21:23:02 +0800

    增加了注册、资产、登录、修改信息界面
    增加了注册、资产、登录、修改信息界面
    优化了三大界面

---
+++ js/merchant.js
@@ -0,0 +1,24 @@
+function jump(k){
+	switch(k){
+		case 1:
+			window.location.href='issue.html';
+			break;
+		case 2:
+			window.location.href='list.html';
+			break;
+		case 3:
+			window.location.href='index.html';
+			break;
+		case 4:
+			window.location.href='edit.html';
+			break;
+		case 5:
+			window.location.href='money.html';
+			break;
+		case 6:
+			$('#repass').popup();
+			break;
+		case 7:
+			window.location.href='login.html';
+	}
+}
